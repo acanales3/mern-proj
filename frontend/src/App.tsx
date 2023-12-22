9fb52b583576eef1df4bf7312967fe306d3b90bd
acanales3 <alexander.s.canales@gmail.com>
2023-12-22 15:09:42 -0500

    Downloaded bootstrap for UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,12 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "react-bootstrap";
 
 function App() {
-  const [clickCount, setClickCount] = useState(0);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world</p>
-        <Button
-          onClick={() => {
-            setClickCount(clickCount + 1);
-          }}
-        >
-          Clicked {clickCount} times
-        </Button>
       </header>
     </div>
   );
