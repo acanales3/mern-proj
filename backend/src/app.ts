a37c5c5ce88e1bdd4b055c26fedd3c5b54f04926
acanales3 <alexander.s.canales@gmail.com>
2023-12-21 20:42:07 -0500

    Added endpoints and middleware for errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,31 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Response, Request } from "express";
+import NoteModel from "./models/note";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// Middleware that gets Note as a JSON
+app.get("/", async (req, res, next) => {
+  try {
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Middleware for route not found if not executed properly funneled into the error block
+app.use((req, res, next) => {
+  next(Error("Endpoint not found"));
+});
+
+// Resuable error catch block as middleware
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+  let errorMessage = "An unknown error occured";
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
