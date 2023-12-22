c55a0b90822735e7fdb10c1bd6ee83c45f69980f
acanales3 <alexander.s.canales@gmail.com>
2023-12-22 11:44:05 -0500

    Added single note retrival functionality

--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -3,7 +3,11 @@ import * as NotesController from "../controllers/notes";
 
 const router = express.Router();
 
+// Get all notes
 router.get("/", NotesController.getNotes);
+
+// Get singular note
+router.get("/:noteId", NotesController.getNote);
 
 // Sedning notes to server
 router.post("/", NotesController.createNote);
