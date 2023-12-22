0c594f055b60438f03c19f5e93c427f5487fa6e5
acanales3 <alexander.s.canales@gmail.com>
2023-12-21 19:53:11 -0500

    Added comments to validateEnv file for envalid

--- backend/src/util/validateEnv.ts
+++ backend/src/util/validateEnv.ts
@@ -1,5 +1,7 @@
 import { cleanEnv, port, str } from "envalid";
 
+// Makes sure our port will always be a number adn the connection string will always be a string
+
 export default cleanEnv(process.env, {
   MONGO_CONNECTION_STRING: str(),
   PORT: port(),
