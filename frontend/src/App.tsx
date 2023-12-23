d408436962ad80ca253f8ef61606d0eaff3a9949
acanales3 <alexander.s.canales@gmail.com>
2023-12-22 19:46:47 -0500

    Added error handeling from load notes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Note as NoteModel } from "./models/note";
 import Note from "./components/Note";
 import { Container, Row, Col } from "react-bootstrap";
 import styles from "./styles/NotesPage.module.css";
+import * as NotesApi from "./network/notes_api";
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
@@ -10,10 +11,7 @@ function App() {
   useEffect(() => {
     async function loadNotes() {
       try {
-        const response = await fetch("/api/notes", {
-          method: "GET",
-        });
-        const notes = await response.json();
+        const notes = await NotesApi.fetchNotes();
         setNotes(notes);
       } catch (error) {
         console.error(error);
