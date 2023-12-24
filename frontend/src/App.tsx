9cf681f410d4ba6697060a20582d91dfdf6d8142
acanales3 <alexander.s.canales@gmail.com>
2023-12-24 12:11:17 -0500

    Edit note functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,13 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 import styles from "./styles/NotesPage.module.css";
 import styleUtils from "./styles/utils.module.css";
 import * as NotesApi from "./network/notes_api";
-import AddNoteDialog from "./components/AddNoteDialog";
+import AddEditNoteDialog from "./components/AddEditNoteDialog";
 import { FaPlus } from "react-icons/fa";
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
   const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
+  const [noteToEdit, setNoteToEdit] = useState<NoteModel | null>(null);
 
   useEffect(() => {
     async function loadNotes() {
@@ -53,19 +54,39 @@ function App() {
             <Note
               note={note}
               className={styles.note}
+              onNoteClicked={setNoteToEdit}
+              // or (note) => {setNoteToEdit(note)} but not needed due to same signature
               onDeleteNoteClicked={deleteNote}
             />
           </Col>
         ))}
       </Row>
       {showAddNoteDialog && (
-        <AddNoteDialog
+        <AddEditNoteDialog
           onDismiss={() => {
             setShowAddNoteDialog(false);
           }}
           onNoteSaved={(newNote) => {
             setNotes([...notes, newNote]);
             setShowAddNoteDialog(false);
+          }}
+        />
+      )}
+      {noteToEdit && (
+        <AddEditNoteDialog
+          noteToEdit={noteToEdit}
+          onDismiss={() => {
+            setNoteToEdit(null);
+          }}
+          onNoteSaved={(updatedNote) => {
+            setNotes(
+              notes.map((existingNote) =>
+                existingNote._id === updatedNote._id
+                  ? updatedNote
+                  : existingNote
+              )
+            );
+            setNoteToEdit(null);
           }}
         />
       )}
--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -31,6 +31,21 @@ export async function createNote(note: NoteInput): Promise<Note> {
     headers: {
       "Content-Type": "application/json",
     },
+    // Sending json data need speicifc header
+    body: JSON.stringify(note),
+  });
+  return response.json();
+}
+
+export async function updateNote(
+  noteId: string,
+  note: NoteInput
+): Promise<Note> {
+  const response = await fetchData("/api/notes/" + noteId, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(note),
   });
   return response.json();
