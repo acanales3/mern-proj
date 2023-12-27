fc8a40404fabe1fb16146cea4998cc2e441655be
acanales3 <alexander.s.canales@gmail.com>
2023-12-27 15:36:07 -0500

    Add userId as part of note schema for auth of users

--- backend/src/models/note.ts
+++ backend/src/models/note.ts
@@ -2,13 +2,13 @@ import { InferSchemaType, Schema, model } from "mongoose";
 
 const noteSchema = new Schema(
   {
+    userId: { type: Schema.Types.ObjectId, required: true },
     title: { type: String, required: true },
     text: { type: String },
   },
   { timestamps: true }
 );
 
-// Create a Type for Typescript and export the model for use
 type Note = InferSchemaType<typeof noteSchema>;
 
 export default model<Note>("Note", noteSchema);
